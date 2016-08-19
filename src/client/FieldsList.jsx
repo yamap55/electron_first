3a6f77384fc8673d558ef1b5cc334aeaee4500ad
yamap55 <tetuya120@hotmail.com>
2016-08-19 19:11:49 +0900

    FieldのCheckBoxリストを表示するように修正。

--- src/client/FieldsList.jsx
+++ src/client/FieldsList.jsx
@@ -6,7 +6,7 @@ class FieldsList extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            options:null,
+            fields:null,
             objectId:props.objectId,
             isBusy:false
         };
@@ -17,31 +17,43 @@ class FieldsList extends React.Component{
             this.state.isBusy = true;
             Login.conn.describe(objectId, (err, meta) => {
                 if (err) { return console.error(err); }
-                const options = meta.fields.filter(
+                const fields = meta.fields.filter(
                     (field)=>{return field.name.toLowerCase() == "id" || field.createable || field.updateable}
                 ).sort((a,b)=>{
                     if( a.label<b.label ) return -1;
                     if( a.label>b.label ) return 1;
                     return 0;
-                }).map((field)=>(<option value={field.name} key={field.name}>{field.label + "（"+field.name+"）"}</option>));
+                }).map((field)=>({name:field.name, label:field.label}));
                 this.state.isBusy = false;
-                this.setState({ options: options});
+                this.state.changed = true;
+                this.setState({ fields: fields});
             });
         }
     }
+
     shouldComponentUpdate(nextProps, nextState) {
       return !(this.state.objectId === nextState.objectId &&
                this.props.objectId === nextProps.objectId &&
-               L.isEqual(this.state.options,nextState.options)
+               L.isEqual(this.state.fields,nextState.fields)
            );
     }
 
     render(){
         if (this.state.isBusy || !this.props.objectId) {
-            return (<select className="form-control" size="10"></select>);
+            // Updating or First Access.
+            return (<div><select className="form-control"></select><div className="fieldsArea"></div></div>);
+        } else if(this.state.changed) {
+            // Fields update.
+            const options = this.state.fields.map((field)=>(<option value={field.name} key={field.name}>{field.label + "（"+field.name+"）"}</option>));
+            const checkboxs = this.state.fields.map((field)=>(
+                <div className="checkbox" key={field.name}><label><input type="checkbox" value={field.name}/>{field.label}</label></div>
+            ));
+            this.state.changed = false;
+            return (<div><select className="form-control">{options}</select><div className="fieldsArea">{checkboxs}</div></div>);
         } else {
+            // Search ObjectId changeed.
             this.getObjectFields(this.props.objectId);
-            return (<select className="form-control" size="10">{this.state.options}</select>);
+            return (<div><select className="form-control"></select><div className="fieldsArea"></div></div>);
         }
     }
 }
